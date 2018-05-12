63e7e0f0cb9d22dbfbf700a6285aafa9ddea1362
JohnnyTheOne <info@johnnytheone.com>
2018-05-12 08:02:06 +0200

    Use Junction Deviation instead

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -434,11 +434,11 @@
 //
 // Use Junction Deviation instead of traditional Jerk Limiting
 //
-//#define JUNCTION_DEVIATION
+#define JUNCTION_DEVIATION
 #if ENABLED(JUNCTION_DEVIATION)
   #define JUNCTION_DEVIATION_FACTOR 0.05
   #define JUNCTION_ACCELERATION_FACTOR 1000
-  //#define JUNCTION_DEVIATION_INCLUDE_E
+  #define JUNCTION_DEVIATION_INCLUDE_E
 #endif
 
 // Microstep setting (Only functional when stepper driver microstep pins are connected to MCU.
