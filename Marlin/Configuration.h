a2c417e8ff1226dc638aa5878bcf8091634134cc
JohnnyTheOne <info@johnnytheone.com>
2018-07-21 10:18:51 +0200

    .

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1249,7 +1249,7 @@
 #define PREHEAT_1_TEMP_BED     70
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
-#define PREHEAT_2_TEMP_HOTEND 235
+#define PREHEAT_2_TEMP_HOTEND 245
 #define PREHEAT_2_TEMP_BED     85
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
