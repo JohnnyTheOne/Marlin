45ec7bc63fc294b2c41555bddd27ee7c7b84e97d
JohnnyTheOne <info@johnnytheone.com>
2018-07-25 21:50:27 +0200

    ACCELERATION

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -627,7 +627,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 400, 400, 10, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 400, 400, 20, 25 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -645,9 +645,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          1000   // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  1000   // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   2000   // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          3000   // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  3000   // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   3000   // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
@@ -898,8 +898,8 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS -2
 #define Z_MIN_POS 0
-#define X_MAX_POS (X_BED_SIZE + 9)
-#define Y_MAX_POS (Y_BED_SIZE + 9)
+#define X_MAX_POS (X_BED_SIZE + 8)
+#define Y_MAX_POS (Y_BED_SIZE + 8)
 #define Z_MAX_POS 250
 
 /**
