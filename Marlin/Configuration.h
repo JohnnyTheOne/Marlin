59d38be9e359809b1a2f7a1f84042afe5f097d62
JohnnyTheOne <info@johnnytheone.com>
2018-11-15 15:07:06 +0100

    SENSORLESS_HOMING

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -928,7 +928,7 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS -20
+#define Y_MIN_POS -17.5
 #define Z_MIN_POS 0
 #define X_MAX_POS (X_BED_SIZE + 7)
 #define Y_MAX_POS (Y_BED_SIZE + 8)
