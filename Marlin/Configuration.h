44eb7912c0ed8994754bc9ec87ec2ff01cdf1143
JohnnyTheOne <info@johnnytheone.com>
2018-11-17 15:43:45 +0100

    PETG

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1321,7 +1321,7 @@
 #define PREHEAT_1_TEMP_BED     70
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "PET-G"
+#define PREHEAT_2_LABEL       "PETG"
 #define PREHEAT_2_TEMP_HOTEND 235
 #define PREHEAT_2_TEMP_BED     85
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
