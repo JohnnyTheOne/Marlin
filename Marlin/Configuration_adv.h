cae2c5fee238d8c15203c62b5124726160bb0e55
JohnnyTheOne <info@johnnytheone.com>
2019-02-10 09:38:51 +0100

    SKR v1.1

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1480,9 +1480,9 @@
    * Override default SPI pins for TMC2130, TMC2160, TMC2660, TMC5130 and TMC5160 drivers here.
    * The default pins can be found in your board's pins file.
    */
-  #define X_CS_PIN          P4_28 //40
-  #define Y_CS_PIN          P2_00 //42
-  #define Z_CS_PIN          P0_19 //65
+  //#define X_CS_PIN          P4_28 //40
+  //#define Y_CS_PIN          P2_00 //42
+  //#define Z_CS_PIN          P0_19 //65
   //#define X2_CS_PIN         -1
   //#define Y2_CS_PIN         -1
   //#define Z2_CS_PIN         -1
@@ -1511,7 +1511,7 @@
    * Use for drivers that do not use a dedicated enable pin, but rather handle the same
    * function through a communication line such as SPI or UART.
    */
-  //#define SOFTWARE_DRIVER_ENABLE
+  #define SOFTWARE_DRIVER_ENABLE
 
   /**
    * TMC2130, TMC2160, TMC2208, TMC5130 and TMC5160 only
@@ -1549,7 +1549,7 @@
    * M912 - Clear stepper driver overtemperature pre-warn condition flag.
    * M122 S0/1 - Report driver parameters (Requires TMC_DEBUG)
    */
-  #define MONITOR_DRIVER_STATUS
+  //#define MONITOR_DRIVER_STATUS
 
   #if ENABLED(MONITOR_DRIVER_STATUS)
     #define CURRENT_STEP_DOWN     50  // [mA]
--- Marlin/src/pins/pins_BIQU_SKR_V1.1.h
+++ Marlin/src/pins/pins_BIQU_SKR_V1.1.h
@@ -321,7 +321,7 @@
     // Example 2. A REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER display is in use. 
     // Note that with this configuration the SD card reader attached to the LCD (if present) can not be
     // used (as the pins will be in use). So LPC_SD_LCD must not be defined.
-    //#define SKR_USE_LCD_SD_CARD_PINS_FOR_CS
+    #define SKR_USE_LCD_SD_CARD_PINS_FOR_CS
     #ifdef SKR_USE_LCD_SD_CARD_PINS_FOR_CS
       #define X_CS_PIN      P0_02
       #define Y_CS_PIN      P0_03
@@ -338,7 +338,7 @@
     // Example 3: We use the driver enable pins for chip select.
     // Note that with this option we must not send commands to the drivers when they are enabled. This means
     // that advanced features like driver monitoring will not be available.
-    #define SKR_USE_ENABLE_CS
+    //#define SKR_USE_ENABLE_CS
     #ifdef SKR_USE_ENABLE_FOR_CS
       #define X_CS_PIN      X_ENABLE_PIN
       #define Y_CS_PIN      Y_ENABLE_PIN
