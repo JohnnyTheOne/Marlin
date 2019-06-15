a02c7471939f81da038ea728d0d7797d6c7e66e2
JohnnyTheOne <info@johnnytheone.com>
2019-06-15 14:36:44 +0200

    Revert "Merge branch 'bugfix-2.0.x' into JTO-A6"
    This reverts commit 6e94c8f32f6dfd53f26247de5dea7a8ea1066148, reversing
    changes made to de4fd89d88ae37d1057ca27d3c4b327532345765.

--- Marlin/src/module/stepper_indirection.cpp
+++ Marlin/src/module/stepper_indirection.cpp
@@ -297,14 +297,11 @@
 
     st.en_pwm_mode(stealth);
 
-    TMC2160_n::PWMCONF_t pwmconf{0};
-    pwmconf.pwm_lim = 12;
-    pwmconf.pwm_reg = 8;
-    pwmconf.pwm_autograd = true;
+    PWMCONF_t pwmconf{0};
+    pwmconf.pwm_freq = 0b01; // f_pwm = 2/683 f_clk
     pwmconf.pwm_autoscale = true;
-    pwmconf.pwm_freq = 0b01;
-    pwmconf.pwm_grad = 14;
-    pwmconf.pwm_ofs = 36;
+    pwmconf.pwm_grad = 5;
+    pwmconf.pwm_ampl = 180;
     st.PWMCONF(pwmconf.sr);
 
     #if ENABLED(HYBRID_THRESHOLD)
@@ -565,14 +562,13 @@
     chopconf.toff = chopper_timing.toff;
     chopconf.hend = chopper_timing.hend + 3;
     chopconf.hstrt = chopper_timing.hstrt - 1;
+    #if ENABLED(SQUARE_WAVE_STEPPING)
+      chopconf.dedge = true;
+    #endif
     st.CHOPCONF(chopconf.sr);
 
-    st.sdoff(0);
     st.rms_current(mA);
     st.microsteps(microsteps);
-    #if ENABLED(SQUARE_WAVE_STEPPING)
-      st.dedge(true);
-    #endif
     st.intpol(INTERPOLATE);
     st.diss2g(true); // Disable short to ground protection. Too many false readings?
 
@@ -655,14 +651,11 @@
 
     st.en_pwm_mode(stealth);
 
-    TMC2160_n::PWMCONF_t pwmconf{0};
-    pwmconf.pwm_lim = 12;
-    pwmconf.pwm_reg = 8;
-    pwmconf.pwm_autograd = true;
+    PWMCONF_t pwmconf{0};
+    pwmconf.pwm_freq = 0b01; // f_pwm = 2/683 f_clk
     pwmconf.pwm_autoscale = true;
-    pwmconf.pwm_freq = 0b01;
-    pwmconf.pwm_grad = 14;
-    pwmconf.pwm_ofs = 36;
+    pwmconf.pwm_grad = 5;
+    pwmconf.pwm_ampl = 180;
     st.PWMCONF(pwmconf.sr);
 
     #if ENABLED(HYBRID_THRESHOLD)
