18c2ff1bdc81ad2a9fb26af3d4aab048d7c1d1a6
JohnnyTheOne <info@johnnytheone.com>
2019-08-20 16:04:55 +0200

    Update pins_BIGTREE_SKR_PRO_V1.1.h

--- Marlin/src/pins/stm32/pins_BIGTREE_SKR_PRO_V1.1.h
+++ Marlin/src/pins/stm32/pins_BIGTREE_SKR_PRO_V1.1.h
@@ -27,7 +27,9 @@
   #error "BIGTREE SKR Pro V1.1 supports up to 3 hotends / E-steppers."
 #endif
 
-#define BOARD_NAME "BIGTREE SKR Pro 1.1"
+#ifndef BOARD_NAME
+ #define BOARD_NAME "BIGTREE SKR Pro 1.1"
+#endif
 
 // Use one of these or SDCard-based Emulation will be used
 //#define SRAM_EEPROM_EMULATION   // Use BackSRAM-based EEPROM emulation
