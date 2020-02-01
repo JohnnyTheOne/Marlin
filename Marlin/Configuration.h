a39623f29ab3f992187002118b433dcef011e078
JohnnyTheOne <info@johnnytheone.com>
2020-02-01 13:06:33 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -731,7 +731,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 2420, 407 } // BMG-813.5
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 2420, 415 } // BMG-813.5
 
 /**
  * Default Max Feed Rate (mm/s)
