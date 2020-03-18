9cf0869b210ef0a1dd40413efcbfed68138808f4
JohnnyTheOne <info@johnnytheone.com>
2020-03-18 17:27:40 +0100

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2321,7 +2321,7 @@
     #define X2_STALL_SENSITIVITY X_STALL_SENSITIVITY
     #define Y_STALL_SENSITIVITY  4
     #define Z_STALL_SENSITIVITY  2
-    //#define SPI_ENDSTOPS              // TMC2130 only
+    #define SPI_ENDSTOPS              // TMC2130 only
     #define IMPROVE_HOMING_RELIABILITY
   #endif
 
