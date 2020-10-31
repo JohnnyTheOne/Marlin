08b1fe7b5abedb10f1765f618d58e466f937aef8
Johnny Eshak <info@johnnytheone.com>
2020-10-31 12:45:04 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1133,7 +1133,7 @@
 #define X_MIN_POS -2
 #define Y_MIN_POS -17.5
 #define Z_MIN_POS 0
-#define X_MAX_POS (X_BED_SIZE + 7)
+#define X_MAX_POS (X_BED_SIZE + 8)
 #define Y_MAX_POS (Y_BED_SIZE + 8)
 #define Z_MAX_POS 240
 
